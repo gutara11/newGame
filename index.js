788adc7d02c5b36fea9c1fd1a10d134e605243ba
Raphael Levy <raffilevy0528@icloud.com>
2021-10-27 12:18:59 -0400

    Changed to a professor theme
    Added logic into javascript to allow multiple professors, changed all the mole theme to professors.

--- index.js
+++ index.js
@@ -4,14 +4,18 @@ const scoreDisplay = document.getElementById('score');
 const holes = document.getElementsByClassName('hole');
 
 setInterval(function() {
+  let proffess = ['prof','prof2', 'prof3'];
+  let choice = proffess[Math.floor(Math.random()*proffess.length)];
   const randomHoleIndex = Math.floor(Math.random() * holes.length);
-  holes[randomHoleIndex].classList.toggle('mole');
+  holes[randomHoleIndex].classList.toggle(choice);
 }, 300);
 
-const gameArea = document.getElementById('whack-a-mole');
+const gameArea = document.getElementById('whack-a-prof');
 gameArea.addEventListener('click', function(clickEvent) {
-  if (clickEvent.target.matches('.mole')) {
-    clickEvent.target.classList.remove('mole');
+  if (clickEvent.target.matches('.prof')||clickEvent.target.matches('.prof2')||clickEvent.target.matches('.prof3')) {
+    clickEvent.target.classList.remove('prof');
+    clickEvent.target.classList.remove('prof2');
+    clickEvent.target.classList.remove('prof3');
     score++;
     scoreDisplay.innerText = score;
   }
