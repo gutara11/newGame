be93cdd76438284b33dd56f4eaabb25dcce110e3
Raphael Levy <raffilevy0528@icloud.com>
2021-11-01 09:15:42 -0400

    Cleaner and Better Code
    Changed a lot of logic so that it is easier to add professors.
    Also made the code look much nicer.
    I gave explanations for each thing.

--- index.js
+++ index.js
@@ -3,20 +3,36 @@ const scoreDisplay = document.getElementById('score');
 
 const holes = document.getElementsByClassName('hole');
 
+let proffess = ['prof','prof2', 'prof3'];
 setInterval(function() {
-  let proffess = ['prof','prof2', 'prof3'];
   let choice = proffess[Math.floor(Math.random()*proffess.length)];
   const randomHoleIndex = Math.floor(Math.random() * holes.length);
   holes[randomHoleIndex].classList.toggle(choice);
 }, 300);
 
+
 const gameArea = document.getElementById('whack-a-prof');
 gameArea.addEventListener('click', function(clickEvent) {
-  if (clickEvent.target.matches('.prof')||clickEvent.target.matches('.prof2')||clickEvent.target.matches('.prof3')) {
-    clickEvent.target.classList.remove('prof');
-    clickEvent.target.classList.remove('prof2');
-    clickEvent.target.classList.remove('prof3');
+  console.log(clickEvent.target.className);
+  //The className will return "hole" if a hole is clicked and '"hole " + classname' if a professor is clicked
+  //so any time a professor is clicked it will be greater than four, otherwise it will equal four or 0
+  //when anything else is clicked it returns 0 (which is less than 4 too)
+  if (clickEvent.target.className.length>4) {
+    remove(clickEvent.target.classList);
     score++;
     scoreDisplay.innerText = score;
   }
 })
+
+//this is so we can remove professors that are added without retyping the remove function every time
+//(the classname returned each time is different so i can't find a solution that will be less than O(N),
+//ideally it should just remove the one prof and be O(1))
+
+function remove(event) {
+  for(let i=0;i<proffess.length;i++){
+    if(i==0)
+      event.remove('prof');
+    else
+      event.remove('prof' +(i+1));
+  }
+}
